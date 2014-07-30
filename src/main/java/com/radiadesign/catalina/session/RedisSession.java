269e19826522e2869d7a2f713df48061c5a45c72
hanyahui <hanyahui88@163.com>
2014-07-30 15:17:34 +0800

    Update RedisSession.java
    redis、java7、tomcat8

--- src/main/java/com/radiadesign/catalina/session/RedisSession.java
+++ src/main/java/com/radiadesign/catalina/session/RedisSession.java
@@ -5,79 +5,79 @@ import org.apache.catalina.Manager;
 import org.apache.catalina.session.StandardSession;
 import java.util.HashMap;
 
-
 public class RedisSession extends StandardSession {
-  protected static Boolean manualDirtyTrackingSupportEnabled = false;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-  public static void setManualDirtyTrackingSupportEnabled(Boolean enabled) {
-    manualDirtyTrackingSupportEnabled = enabled;
-  }
+	protected static Boolean manualDirtyTrackingSupportEnabled = false;
 
-  protected static String manualDirtyTrackingAttributeKey = "__changed__";
+	public void setManualDirtyTrackingSupportEnabled(Boolean enabled) {
+		manualDirtyTrackingSupportEnabled = enabled;
+	}
 
-  public static void setManualDirtyTrackingAttributeKey(String key) {
-    manualDirtyTrackingAttributeKey = key;
-  }
+	protected static String manualDirtyTrackingAttributeKey = "__changed__";
 
+	public void setManualDirtyTrackingAttributeKey(String key) {
+		manualDirtyTrackingAttributeKey = key;
+	}
 
-  protected HashMap<String, Object> changedAttributes;
-  protected Boolean dirty;
+	protected HashMap<String, Object> changedAttributes;
+	protected Boolean dirty;
 
-  public RedisSession(Manager manager) {
-    super(manager);
-    resetDirtyTracking();
-  }
+	public RedisSession(Manager manager) {
+		super(manager);
+		resetDirtyTracking();
+	}
 
-  public Boolean isDirty() {
-    return dirty || !changedAttributes.isEmpty();
-  }
+	public Boolean isDirty() {
+		return dirty || !changedAttributes.isEmpty();
+	}
 
-  public HashMap<String, Object> getChangedAttributes() {
-    return changedAttributes;
-  }
+	public HashMap<String, Object> getChangedAttributes() {
+		return changedAttributes;
+	}
 
-  public void resetDirtyTracking() {
-    changedAttributes = new HashMap<String, Object>();
-    dirty = false;
-  }
+	public void resetDirtyTracking() {
+		changedAttributes = new HashMap<String, Object>();
+		dirty = false;
+	}
 
-  @Override
-  public void setAttribute(String key, Object value) {
-    if (manualDirtyTrackingSupportEnabled && manualDirtyTrackingAttributeKey.equals(key)) {
-      dirty = true;
-      return;
-    }
+	public void setAttribute(String key, Object value) {
+		if (manualDirtyTrackingSupportEnabled
+				&& manualDirtyTrackingAttributeKey.equals(key)) {
+			dirty = true;
+			return;
+		}
 
-    Object oldValue = getAttribute(key);
-    if ( (value != null || oldValue != null)
-         && ( value == null && oldValue != null
-              || oldValue == null && value != null
-              || !value.getClass().isInstance(oldValue)
-              || !value.equals(oldValue) ) ) {
-      changedAttributes.put(key, value);
-    }
+		Object oldValue = getAttribute(key);
+		if (value == null && oldValue != null || oldValue == null
+				&& value != null || !value.getClass().isInstance(oldValue)
+				|| !value.equals(oldValue)) {
+			changedAttributes.put(key, value);
+		}
 
-    super.setAttribute(key, value);
-  }
+		super.setAttribute(key, value);
+	}
 
-  @Override
-  public void removeAttribute(String name) {
-    dirty = true;
-    super.removeAttribute(name);
-  }
+	public void removeAttribute(String name) {
+		dirty = true;
+		super.removeAttribute(name);
+	}
 
-  @Override
-  public void setId(String id) {
-    // Specifically do not call super(): it's implementation does unexpected things
-    // like calling manager.remove(session.id) and manager.add(session).
+	@Override
+	public void setId(String id) {
+		// Specifically do not call super(): it's implementation does unexpected
+		// things
+		// like calling manager.remove(session.id) and manager.add(session).
 
-    this.id = id;
-  }
+		this.id = id;
+	}
 
-  @Override
-  public void setPrincipal(Principal principal) {
-    dirty = true;
-    super.setPrincipal(principal);
-  }
+	public void setPrincipal(Principal principal) {
+		dirty = true;
+		super.setPrincipal(principal);
+	}
 
 }
